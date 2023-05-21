68aabaf8378b5d5c9f04968396a791a46538403c
djimenezchocron <djimenezweb@gmail.com>
2023-05-21 15:19:19 +0200

    Update firebase.config.js

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { collection, getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +23,10 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-// Cloud Firestore
+// Storage
+export const storage = getStorage(app);
+
+// Firestore
 export const db = getFirestore(app);
 export const itemsDB = collection(db, 'items');
 export const usersDB = collection(db, 'users');
